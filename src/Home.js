8a5e5e20a33e96d213bac855163703bea38de52a
richardroy <richardroygibson@gmail.com>
2018-01-14 20:58:40 +1100

    added teh Config object, at the moment it only holds the daly time. Delay can now be customised

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,35 @@
 import Blacklist from "./Blacklist";
+import Config from "./Config";
 
 export default class Home {
   static getBlacklistInputElement() {
     const blackListInput = document.getElementById("blacklistInput");
     return blackListInput;
   }
+
+  static getDelayTimeInputElement() {
+    const delayTimeInput = document.getElementById("delayTimeInput");
+    return delayTimeInput;
+  }
+
+  static getDelayTimeOuputElement() {
+    const delayTimeOutput = document.getElementById("delayTimeOutput");
+    return delayTimeOutput;
+  }
   
   static getBlacklistInputValue() {
     const blackListInput = this.getBlacklistInputElement();
     return blackListInput.value;
+  }
+
+  static getDelayTimeInputValue() {
+    const dealyTimeInput = this.getDelayTimeInputElement();
+    return delayTimeInput.value;
+  }
+
+  static getDelayTimeOutputValue() {
+    const delayTimeOutput = this.getDelayTimeOuputElement();
+    return delayTimeOutput.value;
   }
   
   static clearBlacklistUrlInput() {
@@ -52,15 +73,37 @@ export default class Home {
     Blacklist.addNewUrl(url);  
     this.clearBlacklistUrlInput();
   }
+
+  static setDelayTime() {
+    const time = this.getDelayTimeInputValue();
+    Config.setDelayTime(time);
+    this.setDelayTimeOutputElement(time);
+  }
+
+  static buildInitialBlacklist() {
+    const blacklist = Blacklist.load();
+    for(let index in blacklist) {
+      const url = blacklist[index];
+      this.addNewUrlElementToBlacklist(url);
+    }
+  }
+
+  static setDelayTimeOutputElement(time) {
+    const delayTimeElement = this.getDelayTimeOuputElement();
+    delayTimeElement.textContent = time;  
+  }
+
+  static setInitalDelayTimeElement() {
+    this.setDelayTimeOutputElement(Config.getDelayTime()); 
+  }
 }
 
 document.getElementById("blacklistSubmit").addEventListener("click", () => Home.submitNewUrl());
 document.getElementById("blacklistInput").addEventListener("keypress", (e) => {if(e.keyCode === 13) Home.submitNewUrl();});//submitNewUrl);
 
+document.getElementById("delayTimeSubmit").addEventListener("click", () => Home.setDelayTime());
+
 window.addEventListener("load", function load(event){ 
-  const blacklist = Blacklist.load();
-  for(let index in blacklist) {
-    const url = blacklist[index];
-    Home.addNewUrlElementToBlacklist(url);
-  }
+  Home.buildInitialBlacklist();
+  Home.setInitalDelayTimeElement();
 })
