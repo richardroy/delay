216f3933c73b8478c9c00676c1e63d9bbdf682ab
richardroy <richardroygibson@gmail.com>
2018-01-15 22:41:00 +1100

    blacklist is now an array of objects

--- src/Blacklist.js
+++ src/Blacklist.js
@@ -1,19 +1,37 @@
 import LocalStorage from "./LocalStorage";
-
+import shortId from "shortid";
 const BLACKLIST = "blacklist";
 
 export default class Blacklist {
   static containsUrl(url) {
     const blacklist = this.load();
     for(const listIndex in blacklist) {
-      if(url.includes(blacklist[listIndex]))
+      if(url.includes(blacklist[listIndex].url))
         return true;
     }
     return false;
   }
 
+  static convertOldStructure() {
+    //to persist list over data structure change
+    const newBlacklist = []
+    for(const listIndex in blacklist) {
+      newBlacklist.push({url: blacklist[listIndex], id: shortId.generate()});
+    }
+  
+    this.save(newBlacklist);
+    return this.load();
+  }
+
   static load() {
-    return LocalStorage.loadObject(BLACKLIST, []);
+    const blacklist = LocalStorage.loadObject(BLACKLIST, []);
+    
+    //to persist string list data structure change
+    if(blacklist && typeof blacklist[0] === "string") {      
+      return this.convertOldStructure(black);
+    }
+
+    return blacklist;
   }
   
   static save(blacklist) {
@@ -22,7 +40,7 @@ export default class Blacklist {
 
   static addNewUrl(url) {
     const blacklist = this.load();
-    blacklist.push(url);
+    blacklist.push({url, id: shortId.generate()});
     this.save(blacklist);
   }
 } 
