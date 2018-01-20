fe4726f00ceec68e4276cee247852a6a46e4938b
richardroy <richardroygibson@gmail.com>
2018-01-20 18:03:43 +1100

    removed getUrl and navigateTab from the TabNAvigation as the BrowserService has replaced those functions

--- src/TabNavigation.js
+++ src/TabNavigation.js
@@ -4,37 +4,28 @@ import Delay from "./Delay"
 
 export default class TabNavigation {
 
-  static redirectTab(tabId, url) {
-    BrowserService.updateTabUrl(tabId, url);
-  }
-
-  static getUrl(filename) {
-    const url = BrowserService.getExtensionUrl(filename);
-    return url;
-  }
-
   static getBackgroundUrl() {
-    return this.getUrl("/src/background.html");
+    return BrowserService.getExtensionUrl("/src/background.html");
   }
   
   static getHomeUrl() {
-    return this.getUrl("/src/home.html");  
+    return BrowserService.getExtensionUrl("/src/home.html");    
   }
 
   static redirectToOriginalUrl(tabId) {
     let delay = Delay.loadDelay();
     const site = Delay.getSite(delay, tabId);
-    this.redirectTab(tabId, site.actualUrl);
+    BrowserService.updateTabUrl(tabId, site.actualUrl);
   }
   
   static redirectTabToBackground(tabId) {
     const backgroundPageUrl = this.getBackgroundUrl(); 
-    this.redirectTab(tabId, backgroundPageUrl);          
+    BrowserService.updateTabUrl(tabId, backgroundPageUrl)
   }
   
   static redirectTabToHome(tabId) {
     const homePageUrl = this.getHomeUrl(); 
-    this.redirectTab(tabId, homePageUrl);          
+    BrowserService.updateTabUrl(tabId, homePageUrl);    
   }
 
   static checkUrlAndRedirect(tab) {
