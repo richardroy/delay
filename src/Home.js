b420669a21ee915efdc14c2e2964df429152003e
richardroy <richardroygibson@gmail.com>
2018-01-31 07:46:39 +1100

    added the graph, started building the line data sets

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import Blacklist from "./Blacklist";
+import NavEvents from "./NavEvents";
 import Config from "./Config";
+import Chart from 'chart.js';
 
 export default class Home {
   static getBlacklistInputElement() {
@@ -97,6 +99,98 @@ export default class Home {
     this.setDelayTimeOutputElement(Config.getDelayTime()); 
   }
 }
+
+function getDateString(millis) {
+  const date = new Date(millis);
+  const dateString = date.getFullYear().toString() + date.getMonth().toString() + date.getDate().toString();
+  return dateString;
+}
+  const navEvents = NavEvents.load();  
+  const loadedTimes = [];
+  const navigatedTimes = [];
+  const loadedEvents = {};
+  const navigatedEvents = {};
+  
+
+  for(const index in navEvents) {
+    if(navEvents[index].type === "loaded") {
+      const uniqueDate = getDateString(navEvents[index].time);
+      if(loadedEvents[uniqueDate]) {
+        loadedEvents[uniqueDate].count += 1;
+      } else {
+        loadedEvents[""+uniqueDate] = {}
+        loadedEvents[""+uniqueDate].date = navEvents[index].time;
+        loadedEvents[""+uniqueDate].count = 1;
+      }
+      loadedTimes.push({x: new Date(navEvents[index].time), y:1});
+    } else if(navEvents[index].type === "navigated") {
+      const uniqueDate = getDateString(navEvents[index].time);
+      if(navigatedEvents[uniqueDate]) {
+        navigatedEvents[uniqueDate].count += 1;
+      } else {
+        navigatedEvents[""+uniqueDate] = {}
+        navigatedEvents[""+uniqueDate].date = navEvents[index].time;
+        navigatedEvents[""+uniqueDate].count = 1;
+      }
+      loadedTimes.push({x: new Date(navEvents[index].time), y:1});
+    }
+  }
+
+  const loadedLineData = [];
+  const loadedEventKeys = Object.keys(loadedEvents);
+  for(const index in loadedEventKeys){
+    loadedLineData.push({x: loadedEvents[loadedEventKeys[index]].date, y: loadedEvents[loadedEventKeys[index]].count});
+  }
+
+  const navigatedLineData = [];
+  const navigatedEventKeys = Object.keys(navigatedEvents);
+  for(const index in loadedEventKeys){
+    navigatedLineData.push({x: navigatedEvents[navigatedEventKeys[index]].date, y: navigatedEvents[navigatedEventKeys[index]].count});
+  }
+
+
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          datasets: [{
+              data: loadedLineData,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)'
+              ],
+              borderWidth: 1
+          },{
+            data: navigatedLineData,
+            pointHitRadius: 0,
+            backgroundColor: [
+                'rgba(99, 132, 255, 0.2)'
+            ],
+            borderColor: [
+              'rgba(99, 132, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }],
+          xAxes: [{
+            type: 'time'
+          }]
+        },
+        responsive: false
+      }
+  });
 
 document.getElementById("blacklistSubmit").addEventListener("click", () => Home.submitNewUrl());
 document.getElementById("blacklistInput").addEventListener("keypress", (e) => {if(e.keyCode === 13) Home.submitNewUrl();});//submitNewUrl);
