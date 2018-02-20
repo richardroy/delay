b3803bf0c0e1b4ea36297b4e2bcaf97909433563
richardroy <richardroygibson@gmail.com>
2018-02-20 21:41:04 +1100

    added a NavEventChart file, moved chart functionality to it

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import NavEventChart from './NavEventChart';
 import Blacklist from "./Blacklist";
 import NavEvents from "./NavEvents";
 import Config from "./Config";
@@ -100,75 +101,6 @@ export default class Home {
   }
 }
 
-
-  let { loadedEvents, navigatedEvents } = NavEvents.filterEventList();
-  const loadedLineData = [];
-  const loadedEventKeys = Object.keys(loadedEvents);
-  for(const index in loadedEventKeys){
-    const roundedEventDate = new Date(loadedEvents[loadedEventKeys[index]].date).setHours(0,0,0,0);
-    loadedLineData.push({x: roundedEventDate, y: loadedEvents[loadedEventKeys[index]].count});
-  }
-
-  const navigatedLineData = [];
-  const navigatedEventKeys = Object.keys(navigatedEvents);
-  for(const index in loadedEventKeys){
-    const roundedEventDate = new Date(navigatedEvents[navigatedEventKeys[index]].date).setHours(0,0,0,0);    
-    navigatedLineData.push({x: roundedEventDate, y: navigatedEvents[navigatedEventKeys[index]].count});
-  }
-
-  var ctx = document.getElementById("myChart");
-  var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          datasets: [{
-            data: navigatedLineData,
-            label: '# Navigated',
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)'
-            ],
-            pointBackgroundColor: 'rgba(255,99,132,1)',            
-            borderWidth: 1
-          },{
-            data: loadedLineData,
-            label: '# Loaded',
-            backgroundColor: [
-                'rgba(99, 132, 255, 0.2)'
-            ],
-            borderColor: [
-              'rgba(99, 132, 255, 1)'
-            ],
-            pointBackgroundColor: 'rgba(99, 132, 255, 1)',
-            borderWidth: 1            
-        }]
-      },
-      options: {
-        hover: {
-          mode: 'single'
-        },
-        legend: {
-          display: false
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }],
-          xAxes: [{
-            type: 'time',
-            time: {
-              unit: 'day',
-              tooltipFormat: 'll'
-            }
-          }]
-        },
-        responsive: false
-      }
-  });
-
 document.getElementById("blacklistSubmit").addEventListener("click", () => Home.submitNewUrl());
 document.getElementById("blacklistInput").addEventListener("keypress", (e) => {if(e.keyCode === 13) Home.submitNewUrl();});//submitNewUrl);
 
@@ -178,3 +110,5 @@ window.addEventListener("load", function load(event){
   Home.buildInitialBlacklist();
   Home.setInitalDelayTimeElement();
 });
+
+NavEventChart.initialiseGraph();
