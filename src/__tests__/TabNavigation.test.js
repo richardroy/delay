a16b13ea9f2c9840d7360eae60df13d929f5dba6
richardroy <richardroygibson@gmail.com>
2019-08-17 12:09:50 +0800

    added a new test

--- src/__tests__/TabNavigation.test.js
+++ src/__tests__/TabNavigation.test.js
@@ -8,19 +8,36 @@ import Delay from "../Delay";
 const TAB_ID = 1;
 const BACKGROUND_URL = "background.url";
 const HOME_URL = "home.url";
+const ORIGINAL_URL = "home.url";
 
-const BLACKLIST_ENTRY_REDDIT = {id: 1, url: "wwww.reddit.com", navigatedCount: 0, loadedCount: 0};
+const BLACKLIST_ENTRY_REDDIT = {id: 1, url: ORIGINAL_URL, navigatedCount: 0, loadedCount: 0};
 const DELAY_ENTRY_REDDIT = {"actualUrl":"https://www.reddit.com/","tabId":1405};
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+test("redirectToOriginal", () => {
+  BrowserService.ExtensionUrl = jest.fn().mockReturnValue(ORIGINAL_URL);
+  BrowserService.getTab = jest.fn();
+  Blacklist.addLoadedEvent = jest.fn();
+
+  TabNavigation.redirectToOriginal(TAB_ID, BLACKLIST_ENTRY_REDDIT);
+
+  expect(BrowserService.getExtensionUrl).toHaveBeenCalledWith(BACKGROUND_FILE);
+  expect(BrowserService.getExtensionUrl).toHaveBeenCalledTimes(1);  
+  expect(BrowserService.getTab).toHaveBeenCalledWith(TAB_ID, expect.anything())
+})
 
 test("redirectTabToBackground", () => {
   BrowserService.getExtensionUrl = jest.fn().mockReturnValue(BACKGROUND_URL);
   BrowserService.updateTabUrl = jest.fn();
   TabNavigation.redirectTabToBackground(TAB_ID);
   
-  expect(BrowserService.getExtensionUrl).toHaveBeenCalledTimes(1);  
   expect(BrowserService.getExtensionUrl).toHaveBeenCalledWith(BACKGROUND_FILE);
-  expect(BrowserService.updateTabUrl).toHaveBeenCalledTimes(1);  
+  expect(BrowserService.getExtensionUrl).toHaveBeenCalledTimes(1);  
   expect(BrowserService.updateTabUrl).toBeCalledWith(TAB_ID, BACKGROUND_URL);
+  expect(BrowserService.updateTabUrl).toHaveBeenCalledTimes(1);  
 })
 
 test("redirectTabToHome", () => {
@@ -28,8 +45,8 @@ test("redirectTabToHome", () => {
   BrowserService.updateTabUrl = jest.fn();
   TabNavigation.redirectTabToHome(TAB_ID);
 
-  expect(BrowserService.getExtensionUrl).toHaveBeenCalledTimes(1);  
   expect(BrowserService.getExtensionUrl).toHaveBeenCalledWith(HOME_FILE);
-  expect(BrowserService.updateTabUrl).toHaveBeenCalledTimes(1);  
+  expect(BrowserService.getExtensionUrl).toHaveBeenCalledTimes(1);  
   expect(BrowserService.updateTabUrl).toBeCalledWith(TAB_ID, HOME_URL);
+  expect(BrowserService.updateTabUrl).toHaveBeenCalledTimes(1);  
 })
