738668db207687de5b5034c00b09dbe9e3609525
richardroy <richardroygibson@gmail.com>
2022-01-29 13:55:21 +1100

    updated tests are now passing

--- src/__tests__/TabNavigation.test.js
+++ src/__tests__/TabNavigation.test.js
@@ -8,6 +8,8 @@ const TAB_ID = 1;
 const BACKGROUND_URL = "background.url";
 const HOME_URL = "home.url";
 const ORIGINAL_URL = "home.url";
+const DESIRED_URL = "desired.url";
+const REDIRECT_URL = BACKGROUND_URL + "?" + DESIRED_URL;
 
 const BLACKLIST_ENTRY_REDDIT = {id: 1, url: ORIGINAL_URL, navigatedCount: 0, loadedCount: 0};
 
@@ -34,11 +36,11 @@ test("loadDelayedUrl", () => {
 test("redirectTabToBackground", () => {
   BrowserService.getExtensionUrl = jest.fn().mockReturnValue(BACKGROUND_URL);
   BrowserService.updateTabUrl = jest.fn();
-  TabNavigationService.redirectTabToBackground(TAB_ID);
+  TabNavigationService.redirectTabToBackground(TAB_ID, DESIRED_URL);
   
   expect(BrowserService.getExtensionUrl).toHaveBeenCalledWith(BACKGROUND_FILE);
   expect(BrowserService.getExtensionUrl).toHaveBeenCalledTimes(1);  
-  expect(BrowserService.updateTabUrl).toBeCalledWith(TAB_ID, BACKGROUND_URL);
+  expect(BrowserService.updateTabUrl).toBeCalledWith(TAB_ID, REDIRECT_URL);
   expect(BrowserService.updateTabUrl).toHaveBeenCalledTimes(1);  
 })
 
